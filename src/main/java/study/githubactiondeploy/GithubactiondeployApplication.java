ee9ac10a0858240a0efc5ff4da53581c36f1ab12
jemin <jemin03120111@gmail.com>
2022-01-19 16:35:04 +0900

    initial setting

---
+++ src/main/java/study/githubactiondeploy/GithubactiondeployApplication.java
@@ -0,0 +1,13 @@
+package study.githubactiondeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubactiondeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubactiondeployApplication.class, args);
+    }
+
+}
